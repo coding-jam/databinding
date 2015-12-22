ef44a85dc96121c09b12547b393861692dd1b50c
Fabio Collini <fabio.collini@gmail.com>
2015-12-22 21:46:30 +0100

    Removed existing textChangeListener on binding

--- app/src/main/java/it/cosenonjaviste/databinding/util/Converters.java
+++ app/src/main/java/it/cosenonjaviste/databinding/util/Converters.java
@@ -2,6 +2,7 @@ package it.cosenonjaviste.databinding.util;
 
 import android.databinding.BindingAdapter;
 import android.databinding.BindingConversion;
+import android.support.v4.util.Pair;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -22,13 +23,18 @@ public class Converters {
 
     @BindingAdapter({"app:binding"})
     public static void bindEditText(EditText view, final BindableString bindableString) {
-        if (view.getTag(R.id.bound_observable) != bindableString) {
-            view.setTag(R.id.bound_observable, bindableString);
-            view.addTextChangedListener(new TextWatcherAdapter() {
+        Pair<BindableString, TextWatcherAdapter> pair = (Pair) view.getTag(R.id.bound_observable);
+        if (pair == null || pair.first != bindableString) {
+            if (pair != null) {
+                view.removeTextChangedListener(pair.second);
+            }
+            TextWatcherAdapter watcher = new TextWatcherAdapter() {
                 @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
                     bindableString.set(s.toString());
                 }
-            });
+            };
+            view.setTag(R.id.bound_observable, new Pair<>(bindableString, watcher));
+            view.addTextChangedListener(watcher);
         }
         String newValue = bindableString.get();
         if (!view.getText().toString().equals(newValue)) {
